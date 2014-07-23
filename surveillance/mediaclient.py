1f452a17554ce63a9e8fdb01e9a98968a60b78b7
kadler <spurfan15@gmail.com>
2014-07-22 23:44:47 -0500

    Database class, added to media server / client
    Added a Database class with some initial table schemas and hooked up the
    media server to a few of the Database methods. Added some more
    handshaking between client and server and hooked up image upload in the
    local filesystem as a test.

--- surveillance/mediaclient.py
+++ surveillance/mediaclient.py
@@ -7,38 +7,84 @@ image data to the media server.
 
 import urllib2
 import time
+import json
 
 from cameras.basic_simulator import BasicSimulatorCamera
 
 class MediaClient():
     '''
     Media client class.
-    '''    
+    '''
+    
+    camera_id = 0
     
     @classmethod
-    def start(cls, url, port):
+    def wait_server_ready(cls, base_url):
+        while( True ):
+            req = urllib2.Request(base_url)
+            response = urllib2.urlopen(req)
+            content_type = response.info().getheader( 'Content-Type' )
+            content_length = response.info().getheader( 'Content-Length' )
+            
+            if content_type == 'application/json':
+                json_str = response.read( int( content_length ) )
+                json_dict = json.loads(json_str)
+                
+                if json_dict['ready']:
+                    break
+                
+            time.sleep(5)
+            
+    @classmethod
+    def register_camera(cls, desc, base_url):
+        json_dict = { 'desc' : desc, }
+        json_str = json.dumps( json_dict )
+        
+        url = base_url + '/register'
+        req = urllib2.Request(url)
+        req.add_header( 'Content-Type', 'application/json' )
+        req.add_header( 'Content-length', '{0}'.format( len( json_str ) ) )
+        
+        response = urllib2.urlopen( req )
+        content_type = response.info().getheader( 'Content-Type' )
+        content_length = response.info().getheader( 'Content-Length' )
+            
+        if content_type == 'application/json':
+            json_str = response.read( int( content_length ) )
+            json_dict = json.loads( json_str )
+            cls.camera_id = json_dict['camera_id']
+            
+    @classmethod
+    def send_image(cls, camera, base_url):
+        img = camera.get_image()
+            
+        url = base_url + '/upload/image?camera_id={0}'.format( cls.camera_id )
+        req = urllib2.Request(url, img)
+        req.add_header( 'Cache-Control', 'no-cache' )
+        req.add_header( 'Content-Type', 'image/jpeg' )
+        req.add_header( 'Content-Length', '{0}'.format( len( img ) ) )
+            
+        response = urllib2.urlopen( req )
+        content_type = response.info().getheader( 'Content-Type' )
+        content_length = response.info().getheader( 'Content-Length' )
+            
+        if content_type == 'application/json':
+            json_str = response.read( int( content_length ) )
+            print json_str
+    
+    @classmethod
+    def start(cls, server, port):
         '''
         Start the media client and connect to the server
         at the specified port.
         '''
         
         camera = BasicSimulatorCamera()
+        base_url = '{0}:{1}'.format(server, port)
+        
+        cls.wait_server_ready(base_url)
         
         while( True ):
-            img = camera.get_image()
-            
-            full_url = '{0}:{1}'.format( url, port )
-            req = urllib2.Request(full_url, img)
-            req.add_header( 'Cache-Control', 'no-cache' )
-            req.add_header( 'Content-Type', 'image/jpeg' )
-            req.add_header( 'Content-Length', '{0}'.format( len( img ) ) )
-            
-            response = urllib2.urlopen( req )
-            content_type = response.info().getheader( 'Content-Type' )
-            content_length = response.info().getheader( 'Content-Length' )
-            
-            if content_type == 'application/json':
-                json_str = response.read( int( content_length ) )
-                print json_str
+            cls.send_image(camera, base_url)
             
             time.sleep(5)
--- surveillance/mediaserver.py
+++ surveillance/mediaserver.py
@@ -7,20 +7,54 @@ their media content to a central location.
 
 from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 from SocketServer import ThreadingMixIn
+from urlparse import urlparse, parse_qs
+from database import Database
 
 import cgi
+import datetime
 import json
 
-class MediaHandler(BaseHTTPRequestHandler):
-    # Unique image index
-    image_idx = -1
-          
-    def do_GET(self):
+class MediaApi(object):
+    '''
+    Various classmethods to handle URL parsing and generating
+    proper HTTP response headers and data.
+    '''
+    
+    # The path strings for parsing.
+    path_upload_image    = '/upload/image'
+    path_register_camera = '/register'
+    
+    @classmethod
+    def dispatch(cls, full_path, media_handler):
         '''
-        Media server GET handler for server alive verification.
+        Route a URL to the appropriate handler.
         '''
         
-        json_dict = { 'alive' : 1 }
+        # Parse the URL and get the query dict
+        parse_result = urlparse(full_path)
+        qs_dict = parse_qs(parse_result.query)
+         
+        # get_available_images
+        if parse_result.path == cls.path_upload_image:
+            return cls.upload_image( qs_dict )
+
+class MediaHandler(BaseHTTPRequestHandler):
+    '''
+    Media server handler class.
+    '''
+    
+    # The path strings for parsing.
+    path_upload_image  = '/upload/image'
+    path_camera_register = '/register'
+    
+    def do_GET(self):
+        '''
+        Media server GET handler for server ready verification.
+        '''
+        
+        db = Database.get_instance()
+        
+        json_dict = { 'ready' : db.is_ready(), }
         json_str = json.dumps( json_dict )
         
         self.send_response( 200 )
@@ -36,28 +70,71 @@ class MediaHandler(BaseHTTPRequestHandler):
         Media server POST handler.
         '''
         
+        # Parse the URL and get the query dict
+        parse_result = urlparse(self.path)
+        qs_dict = parse_qs(parse_result.query)
+        
+        if parse_result.path == self.path_camera_register:
+            self.camera_register()
+        elif parse_result.path == self.path_upload_image:
+            self.upload_image( qs_dict )
+            
+    def camera_register(self):
+        # Parse out the content type and length from the headers
+        content_type, _ = cgi.parse_header( self.headers.getheader( 'Content-Type' ) )
+        content_length, _ = cgi.parse_header( self.headers.getheader( 'Content-Length' ) )
+        
+        if content_type == 'application/json':
+            json_str = self.rfile.read( int( content_length ) )
+            json_dict = json.loads(json_str)
+                
+            db = Database.get_instance()
+            camera_id = db.get_camera_id_for_desc(json_dict['desc'])
+            
+            if camera_id == None:
+                camera_id = db.insert_camera(json_dict['desc'])
+            
+            json_dict = { 'camera_id' : id }
+            json_str = json.dumps( json_dict )
+            
+            self.send_response( 200 )
+            self.send_header( 'Content-Type', 'application/json' )
+            self.send_header( 'Content-Length', len( json_str ) )
+            self.end_headers()
+            
+            self.wfile.write( json_str )
+        
+    def upload_image(self, qs_dict ):
         # Parse out the content type and length from the headers
         content_type, _ = cgi.parse_header( self.headers.getheader( 'Content-Type' ) )
         content_length, _ = cgi.parse_header( self.headers.getheader( 'Content-Length' ) )
         
         if content_type == 'image/jpeg':
-            # We can do self.rfile.read() here based on the length
+            img = self.rfile.read( int( content_length ) )
             
-            # Tick the unique image index and form the JSON response
-            # to inform the client of the unique ID assigned to the image.
-            # This unique increment will need to be thread safe.
-            MediaHandler.image_idx += 1
-            json_dict = { 'imgid' : MediaHandler.image_idx, }
+            timestamp = datetime.datetime.now()
+            timestr = timestamp.strftime( '%Y%m%d %H%M%S%f')
+            path = 'images/{0}.jpg'.format( timestr )
+            f = open( path, 'wb' )
+            f.write(img)
+            f.close()
+            
+            db = Database.get_instance()
+            image_id = db.insert_image( int( qs_dict['camera_id'][0] ), 'jpg', path, path, timestamp )
+            
+            print db.get_most_recent_thumbs(20)
+            
+            json_dict = { 'image_id' : image_id, }
             json_str = json.dumps( json_dict )
             
             self.send_response( 200 )
             self.send_header( 'Content-Type', 'application/json' )
-            self.send_header('Content-Length', len( json_str ) )
+            self.send_header( 'Content-Length', len( json_str ) )
             self.end_headers()
         
             # Send out the JSON response
             self.wfile.write( json_str )
-
+            
 class MediaServer(ThreadingMixIn, HTTPServer):
     '''
     Media server.
@@ -65,6 +142,9 @@ class MediaServer(ThreadingMixIn, HTTPServer):
     
     @classmethod
     def start(cls, servers, port):
+        db = Database.get_instance()
+        db.connect('surveillance.db')
+        
         server_address = ('localhost', port)
         httpd = HTTPServer( server_address, MediaHandler )
         servers.append( httpd )
