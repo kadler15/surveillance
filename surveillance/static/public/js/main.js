ed48ffabc8b3babd2a67f900b4f71561494b5089
kadler <spurfan15@gmail.com>
2014-08-02 23:44:49 -0500

    JS Cleanup + Lazy Image Loading
    Organized JS into named functions, used jQuery more, added lazy loading
    of images, using placeholders on initial page updates, and loading
    filling in images as they enter the viewport.

--- surveillance/static/public/js/main.js
+++ surveillance/static/public/js/main.js
@@ -1,29 +1,95 @@
+/* --------------------------------------------------------------------------
+ *  JQuery Ready
+ * --------------------------------------------------------------------------*/
 $(document).ready( function()
     {
-		// Attach a click handler to submit through jQuery
-        $('#submit').click(function() {
-            var start = document.getElementById("start");
-            var end = document.getElementById("end");
-
-			// Form the URL with start & end queries for the available-images call
-            var url = '/availableimages?start=' + start.value + '&end=' + end.value;
-			
-			// Get the images list using an asynchronous call
-            var json = $.ajax(
-                {
-                    url: url,
-                    dataType: 'jsonp',
-                    success: function( json ) {
-						// Loop over the images list and replace the contents of the
-						// internal contents of the images div with the images.
-                        var inner = '';
-                        for( var i = 0; i < json.imgurls.length; i++ )
-                        {
-							inner += '<li><a href="#"><img src="' + json.imgurls[i] + '" alt="description" /><img src="' + json.imgurls[i] + '" alt="description" class="preview" /></a></li>'
-                        }
-                        document.getElementById('images').innerHTML = inner;
-                    }
-                });
-        });
+        $(window).scroll( lazyLoadImages );
+        $(window).resize( lazyLoadImages );
     }
 );
+
+/* --------------------------------------------------------------------------
+ *  Submit Handler
+ * --------------------------------------------------------------------------*/
+function submitClick()
+{
+    var start = document.getElementById("start");
+    var end = document.getElementById("end");
+
+    // Form the URL with start & end queries for the available-images call
+    var url = '/availableimages?start=' + start.value + '&end=' + end.value;
+
+    // Get the images list using an asynchronous call
+    var json = $.ajax(
+        {
+            url: url,
+            dataType: 'jsonp',
+            success: updateWithImages
+        });
+}
+/* --------------------------------------------------------------------------
+ *  Lazy Image Loading
+ * --------------------------------------------------------------------------*/
+function lazyLoadImages()
+{
+    if( $( 'ul.hoverbox > li' ).size() <= 0 ) {
+        return;
+    }
+
+    var data = $( 'ul.hoverbox' ).data();
+
+    for( var i = 0; i < $( 'ul.hoverbox > li' ).size(); i++ ) {
+        var li = $( 'ul.hoverbox > li:nth-child(' + (i+1) + ')' );
+
+        if( li.is( '[blank]' ) && isElementInViewport( li ) ) {
+            li.removeAttr( 'blank' );
+            li.html( '<a href="#"><img src="' + data.imgurls[i] + '" alt="description" /><img src="' + data.imgurls[i] + '" alt="description" class="preview" /></a>' );
+        }
+    }
+}
+
+/* --------------------------------------------------------------------------
+ *  Viewport Helper
+ * --------------------------------------------------------------------------*/
+function isElementInViewport( el ) {
+
+    //special bonus for those using jQuery
+    if (el instanceof jQuery) {
+        el = el[0];
+    }
+
+    var rect = el.getBoundingClientRect();
+
+    return (
+        (
+            (rect.left >= 0 && rect.right <= $(window).width() ) ||               // Inside
+            (rect.left < 0 && rect.right >= 0 ) ||                                // Cross left boundary
+            (rect.left <= $(window).width() && rect.right > $(window).width())    // Cross right boundary
+        ) &&
+        (
+            (rect.top >= 0 && rect.bottom <= $(window).height() ) ||              // Inside
+            (rect.top < 0 && rect.bottom >= 0 ) ||                                // Cross top boundary
+            (rect.top <= $(window).height() && rect.bottom > $(window).height())  // Cross bottom boundary
+         )
+    );
+}
+
+/* --------------------------------------------------------------------------
+ *  Update Helper
+ * --------------------------------------------------------------------------*/
+function updateWithImages( json )
+{
+    // Loop over the images list and append a placeholder for each to
+    // the hoverbox ul. These will be dynamically filled as they are
+    // shown in the viewport.
+    for( var i = 0; i < json.imgurls.length; i++ ) {
+        $( 'ul.hoverbox').append( '<li blank="blank"><a href="#"><img src="" /><img src="" alt="description" class="preview" /></a></li>' );
+    }
+
+    // Setup data on the images list to dynamically load images.
+    $( 'ul.hoverbox' ).data( 'imgurls', json.imgurls );
+
+    // Do an initial call to the scroll handler so the images in the
+    // viewport right now are filled in.
+    lazyLoadImages();
+}
